944738fe38044eb63201705f3224ea4b94e69727
Padma Patil <padma@email.com>
2023-10-29 11:47:51 -0700

    Add gallon to liter and liter to gallon

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//business logic
+function gallonToliter(g) {
+    return g * 3.785;
+}
+
+function literTogallon(l) {
+    return l / 3.785;
+}
+
+//user interface logic
+const g = prompt("Enter a gallon:");
+const l = prompt("Enter a liter:");
+
+window.alert("gallon to liter: " + gallonToliter(g) + ".\n" +
+    "liter to gallon: " + literTogallon(l) + ".");
